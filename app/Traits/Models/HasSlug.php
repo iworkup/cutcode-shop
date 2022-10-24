acedff78baa00b82879a603b899d99c4357a1c0a
Igor <iworkup@ya.ru>
2022-10-24 21:47:27 +0300

    2 lesson. 4 video.

---
+++ app/Traits/Models/HasSlug.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Traits\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait HasSlug
+{
+
+    protected static function bootHasSlug()
+    {
+        parent::boot();
+
+        static::creating(function (Model $model) {
+            $model->slug = $model->slug ?? str($model->{self::slugFrom()})->append(time())->slug();
+        });
+    }
+
+    public static function slugFrom(): string
+    {
+        return 'title';
+    }
+
+
+}
